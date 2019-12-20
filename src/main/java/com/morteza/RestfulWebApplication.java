96e8cd978dae3d6d3b39e2e5db0e5d3300dd5371
mortezab89 <morteza.bandi@gmail.com>
2019-12-20 16:32:51 +0330

    * internationalization problem fixed * multiple basenames are handled

--- src/main/java/com/morteza/RestfulWebApplication.java
+++ src/main/java/com/morteza/RestfulWebApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
+import java.util.Set;
 
 @SpringBootApplication
 public class RestfulWebApplication {
@@ -24,10 +26,21 @@ public class RestfulWebApplication {
         return sessionLocaleResolver;
     }
 
-    @Bean(name = "messageBean")
-    public ResourceBundleMessageSource resourceBundleMessageSource(){
-        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
-        resourceBundleMessageSource.setBasename("messages");
-        return resourceBundleMessageSource;
+    @Bean
+    public ResourceBundleMessageSource resourceBundleMessageSourceForMessagesFile(){
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("messages","errors");
+        messageSource.setDefaultEncoding("UTF-8");
+
+        Set<String> basenameSet = messageSource.getBasenameSet();
+        return messageSource;
     }
+
+//    public ResourceBundleMessageSource resourceBundleMessageSourceForErrorFile(){
+//        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+//        resourceBundleMessageSource.setBasename("classpath:errors");
+//        resourceBundleMessageSource.setDefaultEncoding("UTF-8");
+//        return resourceBundleMessageSource;
+//    }
+
 }
--- src/main/java/com/morteza/helloworld/HelloWorldController.java
+++ src/main/java/com/morteza/helloworld/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.morteza.helloworld;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
@@ -10,7 +11,8 @@ import java.util.Locale;
 public class HelloWorldController {
 
     @Autowired
-    private MessageSource messageSource;
+    private ResourceBundleMessageSource messageSource;
+
 
 //    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     @GetMapping(path = "/hello-world")
@@ -29,7 +31,12 @@ public class HelloWorldController {
     }
 
     @GetMapping("/hello-world-internationalization")
-    public String hellowWorldInternationalization(@RequestHeader(name = "Accept-Language", required = false) Locale locale){
+    public String helloWorldInternationalization(@RequestHeader(name = "Accept-Language", required = false) Locale locale){
         return messageSource.getMessage("good.morning.message", null, locale);
+    }
+
+    @GetMapping("/error")
+    public String error(@RequestHeader(name = "Accept-Language", required = false) Locale locale){
+        return messageSource.getMessage("error", null, locale);
     }
 }
